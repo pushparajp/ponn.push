e495e07a992b208bbf93df130579877c1ebfddab
Pushparaj <pushparaj@axaratech.com>
2012-05-03 20:44:04 +0530

    remove test_app

--- test_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TestApp::Application.config.secret_token = 'b07d8bd7925db5ee17cb3338fae8deae3d7d88dae0e07882a4430fcdc907ca28ed290493ee1ca7ee10c30f3e4533884a49eb544672662c1d8ecbfb7bf6ae3214'
